aac32c691a8fe6924702bbff04ffc547c59bfe8e
Amir Maralani <amir.maralani@gmail.com>
2019-07-17 16:36:41 +0430

    Added JaCoCo

--- src/test/java/ir/maralani/pixy/PixyApplicationTests.java
+++ src/test/java/ir/maralani/pixy/PixyApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("dev") 
+@ActiveProfiles("dev")
 public class PixyApplicationTests {
 
     @Test
